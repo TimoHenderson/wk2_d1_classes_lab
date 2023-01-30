706fe3f688b1255b8b43534b305f1068cec87df8
TimoHenderson <tjhenderson2@gmail.com>
2023-01-30 15:09:35 +0000

    Added gitignore - removed pycache

--- team_class/src/team.py
+++ team_class/src/team.py
@@ -3,9 +3,14 @@ class Team:
         self.name = name
         self.players = players
         self.coach = coach
+        self.points = 0
 
     def add_player(self, new_player):
         self.players.append(new_player)
 
     def has_player(self, player_name):
         return player_name in self.players
+
+    def play_game(self, won_game):
+        if won_game:
+            self.points += 3
