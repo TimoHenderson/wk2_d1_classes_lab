a8de9e0e383814cfab69a9450026616881cc2587
TimoHenderson <tjhenderson2@gmail.com>
2023-01-30 14:52:13 +0000

    Add Team class

--- team_class/tests/team_test.py
+++ team_class/tests/team_test.py
@@ -1,24 +1,21 @@
 import unittest
 from src.team import Team
 
+
 class TestTeam(unittest.TestCase):
     def setUp(self):
         players = ["Derice Bannock", "Sanka Coffie", "Junior Bevil", "Yul Brenner"]
         self.team = Team("Cool Runnings", players, "Irv Blitzer")
 
-    @unittest.skip("delete this line to run the test")
     def test_team_has_name(self):
         self.assertEqual("Cool Runnings", self.team.name)
 
-    @unittest.skip("delete this line to run the test")
     def test_team_has_players(self):
         self.assertEqual(4, len(self.team.players))
 
-    @unittest.skip("delete this line to run the test")
     def test_team_has_coach(self):
         self.assertEqual("Irv Blitzer", self.team.coach)
 
-    @unittest.skip("delete this line to run the test")
     def test_coach_can_be_changed(self):
         self.team.coach = "John Candy"
         self.assertEqual("John Candy", self.team.coach)
@@ -37,12 +34,12 @@ class TestTeam(unittest.TestCase):
     def test_check_player_in_team__not_found(self):
         self.assertEqual(False, self.team.has_player("Usain Bolt"))
 
-#   Extensions
-#   Hint You do not need to add 'points' as a parameter in your '__init__' function inside your 'Team' class. You can still add it as a class properties. 
+    #   Extensions
+    #   Hint You do not need to add 'points' as a parameter in your '__init__' function inside your 'Team' class. You can still add it as a class properties.
     @unittest.skip("delete this line to run the test")
     def test_team_has_points(self):
         self.assertEqual(0, self.team.points)
-        
+
     @unittest.skip("delete this line to run the test")
     def test_play_game__win(self):
         self.team.play_game(True)
